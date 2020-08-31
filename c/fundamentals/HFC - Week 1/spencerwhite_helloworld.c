bca4499412dea17c5d628a487a1dedd2985824e1
Spencer White <swhit210@ford.com>
2020-08-31 18:34:42 -0400

    hello world example

---
+++ c/fundamentals/HFC - Week 1/spencerwhite_helloworld.c
@@ -0,0 +1,19 @@
+/*
+   Spencer White
+   CIS 170-70 - Hello World
+   Prints "Hello, Spencer!" to the console
+*/
+
+#include <stdio.h>
+
+/*
+   Function Description - Prints "Hello, Spencer!"" to the console
+   Parameters: N/A
+   Returns: N/A
+*/
+
+void main() {
+
+   printf("Hello, Spencer!\n");
+
+}
--- c/fundamentals/helloworld.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-
-   int playerScore = 95;
-   // printf() displays the string inside quotation
-   printf("Hello, World!\n");
-   return 0;
-}
