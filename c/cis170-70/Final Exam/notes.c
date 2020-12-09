5fe91d61efa002399f3e26a962fefa7aacb2df36
Spencer White <swhit210@ford.com>
2020-12-08 19:29:10 -0500

    reduction to focus on essential tasks

--- c/cis170-70/Final Exam/notes.c
+++ c/cis170-70/Final Exam/notes.c
@@ -24,9 +24,7 @@ struct Node {
 // #3B - Move - Free the Memory When Appropriate - DONE
 // #4 - Update - Add Color - Set colorName, colorCode, and hasColor - DONE     
 // #5 - Update - Remove Color - set colorValue, colorCode, and hasColor - DONE
-// #6 - Print Linked List with Colors In Terminal (ANSI)
-// #7 - Form Prompt That Renders Correct as PS1= (Custom Content + COLORS)
-// #8 - Append Special Variable Content
+// #8 - Append Special Variable Content - set isSpecial, textValue
 // #9 - Print Linked List with Colors and Special Variable Content (ANSI + Special Variable Examples)
 // #10 - Form Prompt That Renders Correct as PS1= (Custom Content + COLORS + Special Variables)
 // #11 - Write Prompt to File
