ae7b4c18d5cbfeb9b2fd5770978607631fec8bd6
Spencer White <swhit210@ford.com>
2021-01-14 11:46:56 -0500

    example of arithmetic operators

---
+++ c++/cis230-01/HFC - Week 1/arithmetic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    int sum = 1 + 5;
+    int difference = 1 - 5;
+    int quotient = 5 / 1;
+    int product = 1 * 5;
+    int remainder = 10 * 3;
+
+    return 0;
+}
