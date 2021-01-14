6bf6d131bfb1d6d6f5b8eb0dd302f5f7490f393a
Spencer White <swhit210@ford.com>
2021-01-14 11:47:55 -0500

    examples with cout

---
+++ c++/cis230-01/HFC - Week 1/cout.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main () {
+
+    std::cout<<"Programming is great fun!\n"; // << - stream insertion operatorw
+
+    std::cout<<"Programming is "<<"great fun!\n";
+
+    std::cout<<"Programming is ";
+    std::cout<<"great fun!\n";
+
+    std::cout<<"Programming is great fun!"<<std::endl;
+    std::cout<<"Programming is great fun!\t TABBED"<<std::endl;
+    std::cout<<"Programming is great fun!\a ALARMED"<<std::endl;
+    std::cout<<"Programming is great fun!\b BACKED"<<std::endl;
+    std::cout<<"Programming is great fun!\r RETURNED"<<std::endl;
+    std::cout<<"Programming is great fun!\\ BACKSLASH"<<std::endl;
+    std::cout<<"Programming is great fun!\' SINGLE QUOTE"<<std::endl;
+    std::cout<<"Programming is great fun!\" DOUBLE QUOTE"<<std::endl;
+
+    return 0;
+}
