194bd8763dba4ac024fbfd90e9e0879902a537c3
Spencer White <swhit210@ford.com>
2021-01-14 11:48:12 -0500

    example of variable and literals

---
+++ c++/cis230-01/HFC - Week 1/variables_assignment.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    // Variable Declaration
+    int num;
+
+    // Literal Assignment - a piece of data that is written directly into a program's code
+    num = 5;
+
+    return 0;
+}
