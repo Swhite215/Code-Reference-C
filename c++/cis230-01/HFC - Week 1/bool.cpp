d716bbfba9039db0fe419301ca36c189efa187dc
Spencer White <swhit210@ford.com>
2021-01-14 11:47:20 -0500

    example of boolean data type

---
+++ c++/cis230-01/HFC - Week 1/bool.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main () {
+
+    bool boolValue;
+    boolValue = true; // Stored as 1 = true or 0 = False
+
+    std::cout<<"It is "<<boolValue<<" you are awesome!"; // Will print a 1
+
+    return 0;
+}
