bf2c0efc5c8c9f630785d539cc72fab615d2db84
Spencer White <swhit210@ford.com>
2021-01-14 11:47:38 -0500

    example of sizeof

---
+++ c++/cis230-01/HFC - Week 1/sizeof.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    double apple;
+
+    std::cout<<"The size of a short integer is "<< sizeof(short) << " bytes."<<std::endl;
+    std::cout<<"The size of a long integer is "<< sizeof(long) << " bytes."<<std::endl;
+    std::cout<<"An apple can be eaten in  "<< sizeof(apple) << " bytes!"<<std::endl;
+
+    return 0;
+}
