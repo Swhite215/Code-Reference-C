f56212c5b8b216978c39aff576943bf835b6be0f
Spencer White <swhit210@ford.com>
2021-01-14 11:47:30 -0500

    example of string class

---
+++ c++/cis230-01/HFC - Week 1/string_class.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string> // header file for storing and working with strings
+
+int main() {
+
+    std::string movieTitle;
+    movieTitle = "Wheels of Fury";
+
+    std::cout<< "My favorite movie is "<< movieTitle << std::endl;
+
+    return 0;
+}
