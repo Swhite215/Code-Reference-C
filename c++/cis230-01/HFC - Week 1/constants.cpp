63d3f92efa7afe888f14c418bae879d249076b69
Spencer White <swhit210@ford.com>
2021-01-15 12:00:11 -0500

    constants examples

---
+++ c++/cis230-01/HFC - Week 1/constants.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main () {
+
+    const double INTEREST_RATE = 0.069; // Must be initialized with value
+    const double EULERS_NUMBER = 2.71828;
+    const double MINUTES_IN_YEAR = 5.256e5;
+    const double GRAVITY_IN_FEET = 32.2;
+    const double GRAVITY_IN_METERS = 9.8;
+    const int METERS_IN_MILE = 1609;
+
+    return 0;
+}
