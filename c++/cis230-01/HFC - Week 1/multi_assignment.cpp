c4e7df7ac04326a412e6790a9667c071ccacdc14
Spencer White <swhit210@ford.com>
2021-01-15 12:00:37 -0500

    example of multi assignment

---
+++ c++/cis230-01/HFC - Week 1/multi_assignment.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main () {
+
+    int number = 10;
+
+    number = number - 10;
+    number = number + 90;
+    number = number * 3;
+    number = number / 1;
+    number += 5;
+    number -= 1;
+    number *= 2;
+    number /= 1;
+
+    return 0;
+}
