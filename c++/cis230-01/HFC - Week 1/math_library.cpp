729c9e5e79a04b84325b13f6b3be6d839d543df5
Spencer White <swhit210@ford.com>
2021-01-15 15:17:03 -0500

    math library functions

---
+++ c++/cis230-01/HFC - Week 1/math_library.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+int main () {
+
+    int y = abs(-4);
+    double cosA = cos(12);
+    double exponential = exp(10);
+    double remainder = fmod(10, 3);
+    double logA = log(1);
+    double logB = log10(1);
+    double power = pow(3, 2);
+    int rounded = round(10.11);
+    double sinA = sin(12);
+    double squareRoot = sqrt(4);
+    double tanA = tan(12);
+
+    return 0;
+}
