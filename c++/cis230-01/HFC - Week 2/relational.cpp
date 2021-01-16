1ef7e45ea60fc00fc09871e296b9eff763a455cb
Spencer White <swhit210@ford.com>
2021-01-16 15:38:34 -0500

    example relational operators

---
+++ c++/cis230-01/HFC - Week 2/relational.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    // 0 for false and 1 for true
+    std::cout<<(1 > 3)<<std::endl;
+    std::cout<<(1 < 3)<<std::endl;
+    std::cout<<(1 >= 3)<<std::endl;
+    std::cout<<(1 <= 3)<<std::endl;
+    std::cout<<(1 == 3)<<std::endl;
+    std::cout<<(1 != 3)<<std::endl;
+
+    return 0;
+}
