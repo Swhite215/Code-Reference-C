3a257bf5ad938d9a8a4bf9f67be8d3d167fec2aa
Spencer White <swhit210@ford.com>
2021-01-16 15:38:26 -0500

    example logical operators

---
+++ c++/cis230-01/HFC - Week 2/logical.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+    // 0 for false and 1 for true
+    std::cout<<(1 > 3 && 7 < 9)<<std::endl;
+    std::cout<<(1 < 3 || 4 == 4)<<std::endl;
+    std::cout<<(!(1 >= 3))<<std::endl;
+    
+    return 0;
+}
