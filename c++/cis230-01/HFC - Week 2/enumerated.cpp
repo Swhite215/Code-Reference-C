330c3623cfab27f63853ca2d18ab200ce1c62411
Spencer White <swhit210@ford.com>
2021-01-16 15:38:13 -0500

    example enum

---
+++ c++/cis230-01/HFC - Week 2/enumerated.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+
+    enum Roster { Tranquility, Joxos, Beatrix, Vi, Emory, Flare, Inferno, Armadias };
+    std::cout<<Tranquility<<std::endl;
+    return 0;
+}
