120bce4d1c41d4b9969018be00b9e9b6dc399a7a
Spencer White <swhit210@ford.com>
2021-02-10 10:03:17 -0500

    initial attempt at person assignment

---
+++ c++/cis230-01/HFC - Week 5/Person.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class Person {
+    private:
+        std::string lastName;
+        std::string firstName;
+
+    public:
+        Person() {
+            lastName = " ";
+            firstName = " ";
+        }
+
+        Person(std::string l, std::string f) {
+            lastName = l;
+            firstName = f;
+        }
+
+        void displayPerson() {
+            std::cout<<lastName<<", "<<firstName<<std::endl;
+        }
+};
+
+int main() {
+
+    Person p1 = Person("White", "Spencer");
+    p1.displayPerson();
+
+    return 0;
+}
