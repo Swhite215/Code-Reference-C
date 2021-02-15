77b604342710aac4e43c047f24cd054b4d03ffe1
Spencer White <swhit210@ford.com>
2021-02-15 10:02:45 -0500

    example using typedef

---
+++ c++/cis230-01/HFC - Week 6/typedef.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    // typedef <existing data type> <alias>
+    typedef int examScore;
+    examScore score1, score2;
+
+    typedef double score[100];
+    double finalExam[100];
+    score finalExams[100];
+
+    return 0;
+}
