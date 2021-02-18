4f8cd86e98a2473c154eaa005c755883a13d0e58
Spencer White <swhit210@ford.com>
2021-02-18 11:55:20 -0500

    update to published book

--- c++/cis230-01/HFC - Week 4/spencerwhite_publishedbook/Author.cpp
+++ c++/cis230-01/HFC - Week 4/spencerwhite_publishedbook/Author.cpp
@@ -2,10 +2,10 @@
 #include "Author.h"
 
 Author::Author(std::string f, std::string l, char m) {
-            firstName = f;
-            lastName = l;
-            middleInitial = m;
-        }
+    firstName = f;
+    lastName = l;
+    middleInitial = m;
+}
 
 void Author::displayAuthor() {
     std::cout<<lastName<<", "<<firstName<<" "<<middleInitial<<".";
