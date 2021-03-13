b4d12f176c410cc3a49687a43385334f772688ae
Spencer White <swhit210@ford.com>
2021-03-13 15:10:05 -0500

    finsihed last required functions

--- c++/cis230-01/HFC - Week 8/spencerwhite-midterm/CreditCard.cpp
+++ c++/cis230-01/HFC - Week 8/spencerwhite-midterm/CreditCard.cpp
@@ -1,6 +1,8 @@
 #include "CreditCard.h"
-#include <iostream>
+#include "Transaction.h"
 #include <vector>
+#include <iomanip>
+#include <iostream>
 
 CreditCard::CreditCard(std::string n, std::string a, std::string c , std::string s, int z, long int ss): customer(n, a, c , s, z, ss) {
 
@@ -55,5 +57,65 @@ void CreditCard::determineLimitAndInterest() {
 long int CreditCard::getAccountNumber() {
     return accountNumber;
 }
+
+/*
+   Function Description: Handles Adding a Transaction
+   Parameters: N/A
+   Returns: long int accountNumber
+*/
+void CreditCard::addTransaction(Transaction tx) {
+    // Get Transaction Amount
+    float amount = tx.getAmount();
+
+    // Get Current Balance
+    double amountAvailable = creditLimit - balance;
+
+    if (amountAvailable >= amount) {
+        balance += amount;
+        transactions.push_back(tx);
+    } else {
+        std::cout<<"Transaction declined. You don't have enough remaining balance."<<std::endl;
+    }
+}
+
+/*
+   Function Description: Displays Credit Card Account Information
+   Parameters: N/A
+   Returns: N/A
+*/
+void CreditCard::displayInfo() {
+    double amountAvailable = creditLimit - balance;
+    std::cout<<"Credit Card - Account Number: "<<accountNumber<<" Balance: "<<balance<<" Available Credit: $"<<std::fixed<<std::setprecision(2)<<amountAvailable<<"."<<std::endl;
+}
+
+/*
+   Function Description: Displays Credit Card Statement
+   Parameters: N/A
+   Returns: N/A
+*/
+void CreditCard::printStatement() {
+
+    std::cout<<"Credit Card - Account Statement"<<std::endl;
+
+    // Display Account Holder Information
+    std::cout<<"Account Holder: ";
+    customer.displayInfo();
+
+    // Display Transactions
+    std::cout<<"Transactions: "<<std::endl;
+
+    for (Transaction tx : transactions) {
+        tx.display();
+    }
+
+    // Display Total Interest Due
+    double interestDue = balance * interestPercentage;
+    std::cout<<"Total Interest Due: $"<<std::fixed<<std::setprecision(2)<<interestDue<<"."<<std::endl;
+
+    // Display Total Balance Due
+    double totalBalanceDue = balance += interestDue;
+    std::cout<<"Total Balance Due: $"<<std::fixed<<std::setprecision(2)<<totalBalanceDue<<"."<<std::endl;
+}
+
 
 long int CreditCard::newAccountNumber = 100200;
